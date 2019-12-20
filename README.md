b2808ba720bf2d7503d8a5e8760e7d09cf7c797f
nihar-sonawane <nihar.sonawane@nineleaps.com>
2019-12-20 13:20:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# horizontal-stepper
+
+horizontal-stepper is a simple stepper component using react, which takes an array of steps title and a function that returns the body to be displayed in the stepper at every step.
+Input - 1) steps title in string array
+        2) stepContent function that returns the body to be displayed in the stepper at every step.
+
+## Installation
+
+`yarn add horizontal-stepper`
+
+## Usage
+
+```jsx
+
+```
+
+## Development
+    yarn
+    yarn dev
+
+## Test
+    yarn test
+
+## Build
+    yarn
+    yarn build
+
+## Publish
+    npm login
+    npm version patch
+    git add -A
+    git push origin master
+    npm publish
---
+++ src/Stepper.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react";
+import $ from 'jquery';
+import Stepper from "./Stepper";
+
+class Test extends Component {
+  
+  constructor(props) {
+    super(props);
+    this.state = ({
+      activeStep: 1,
+      completed: 0
+    })
+  }
+
+  handleOnClickStepper = (step) => {
+    const completed = this.state.completed;
+    this.setState({
+      completed: completed,
+      activeStep: step,
+    });
+  }
+
+  handleOnNextClick = () => {
+    let nextStep = this.state.activeStep + 1;
+    if (nextStep === this.props.steps.length) {
+      this.setState({
+        activeStep: nextStep,
+        completed: this.props.steps.length
+      })
+    }
+    else if (this.state.activeStep <= this.state.completed) {
+      const completed = this.state.completed;
+      this.setState({
+        completed: completed,
+        activeStep: nextStep
+      })
+    }
+    else {
+      this.setState({
+        activeStep: nextStep,
+        completed: nextStep - 1
+      })
+    }
+  }
+
+  handleFinish = () => {
+    console.log('Finished');
+  }
+
+  handleOnClickBack = () => {
+    let prevStep = this.state.activeStep - 1;
+    const completed = this.state.completed;
+    this.setState({
+      completed: completed,
+      activeStep: prevStep,
+    })
+  }
+
+  componentDidMount = () => {
+    document.getElementsByClassName('stepper-item-inner-active');
+    $('.stepper-item-inner-active').parent().addClass('sizeparent');
+    $('.stepper-item-inner-active').parent().css('pointer-events', 'auto');
+    for (let i = 1; i < this.state.activeStep; i++) {
+      $(`.${i}`).addClass('completed');
+    }
+    for (let i = 0; i < this.state.completed; i++) {
+      $('.stepper-item-inner').eq(i).addClass('step-completed');
+      $('.step-completed').parent().css('pointer-events', 'auto');
+    }
+  }
+
+  render() {
+  return (
+          <div className="stepper">
+            <Stepper
+              steps={this.props.steps}
+              activeStep={this.state.activeStep}
+              onSelect={this.handleOnClickStepper}
+              showNumber={false} />
+            <div className="stepper-body">
+              {this.props.stepContent(this.state.activeStep - 1)}
+              <div className="buttons" style={{ marginTop: '40px' }}>
+                {this.state.activeStep === 1 ? <input type="button" disabled className="previous-button" value="Back" onClick={this.handleOnClickBack} /> : <input type="button" className="previous-button" value="Back" onClick={this.handleOnClickBack} />}
+                <input type="button" className={this.state.activeStep === this.props.steps.length ? "finish-button" : "next-button"} value={this.state.activeStep === this.props.steps.length ? 'Finish' : 'Next'}
+                  onClick={this.state.activeStep === this.props.steps.length ? handleFinish : handleOnNextClick} />
+              </div>
+            </div>
+          </div>
+  );
+  }
+}
+
+export default Test;
